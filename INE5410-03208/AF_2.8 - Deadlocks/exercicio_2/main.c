210f55d99ab7d666aaf0c59a7d2186dc0840b4a6
Leonardo Brito <92063418+Leoguibrito@users.noreply.github.com>
2022-10-26 13:37:39 -0300

    Update main.c

--- INE5410-03208/AF_2.8 - Deadlocks/exercicio_2/main.c
+++ INE5410-03208/AF_2.8 - Deadlocks/exercicio_2/main.c
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include "banco.h"
 
-
 // Faz uma transferência de valor unidades monetárias da conta origem
 // para a conta destino. Se a conta origem tiver saldo insuficiente, o
 // saldo fica negativo, simbolizando cheque especial.  
@@ -14,11 +13,6 @@
 // sincronização e pode produzir valores errados se utilizada
 // concorrentemente com a mesma conta.
 //
-
-int origem_ant = 0;
-int destino_ant = 0;
-sem_t semaforo;
-
 // Definida em helper.c
 extern void transferir_unsafe(conta_t *origem, conta_t *destino, double valor);
 
@@ -27,43 +21,31 @@ void transferir(conta_t *origem, conta_t *destino, double valor);
 // Thread que faz  transferências aleatórias. Definida em helper.c
 void* caixa_func(void *arg);
 
+
+
 // Versão thread-safe da função transferir_unsafe.
 void transferir(conta_t *origem, conta_t *destino, double valor) {
-    if (origem -> id == destino_ant ||  origem_ant == destino -> id || origem -> id == origem_ant) {
-        origem_ant = origem -> id;
-        destino_ant = destino -> id;
-
-        sem_wait(&semaforo);
-        pthread_mutex_lock(&origem->mutex);
-        pthread_mutex_lock(&destino->mutex);
-
-        transferir_unsafe(origem, destino, valor);
-
-        pthread_mutex_unlock(&origem->mutex);
-        pthread_mutex_unlock(&destino->mutex);
-        sem_post(&semaforo);
-
-    } else {
-        origem_ant = origem -> id;
-        destino_ant = destino -> id;
-
-        pthread_mutex_lock(&origem->mutex);
-        pthread_mutex_lock(&destino->mutex);
-
-        transferir_unsafe(origem, destino, valor);
-
-        pthread_mutex_unlock(&origem->mutex);
-        pthread_mutex_unlock(&destino->mutex);
-    }
-
+    pthread_mutex_t *maior_id;
+    pthread_mutex_t *menor_id;
     
-}
+    if (origem -> id > destino -> id) {
+        maior_id = &origem->mutex;
+        menor_id = &destino->mutex;
+    } else {
+        maior_id = &destino->mutex;
+        menor_id = &origem->mutex;
+    }
+    pthread_mutex_lock(maior_id);
+    pthread_mutex_lock(menor_id);
 
+    transferir_unsafe(origem, destino, valor);
+
+    pthread_mutex_unlock(menor_id);
+    pthread_mutex_unlock(maior_id);
+}
 
 int main(int argc, char* argv[]) {
     // Cria as contas com algum saldo e inicializa o mutex de cada conta
-    sem_init(&semaforo,0,1);
-
     for (int i = 0; i < NUM_CONTAS; i++) {
         contas[i].id = i;
         contas[i].saldo = 1000.00;
@@ -82,7 +64,6 @@ int main(int argc, char* argv[]) {
     // Destroi os mutexes
     for (int i = 0; i < NUM_CONTAS; i++)
         pthread_mutex_destroy(&contas[i].mutex);
-
-    sem_destroy(&semaforo);  
+  
     return 0;
 }
